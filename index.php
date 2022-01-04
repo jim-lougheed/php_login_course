ddea8bb824c4deb2e90eef0cf6cf28295dfa9209
Jim Lougheed <jims.codes@gmail.com>
2022-01-04 15:25:52 -0500

    Login page and HTML setup

--- index.php
+++ index.php
@@ -11,6 +11,32 @@
 </head>
 <body>
 
+<div class="uk-section uk-container">
+    <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid=''>
+        <form class="uk-form-stacked js-login">
+
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-text">Email</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+                </div>
+            </div>
+
+            <div class="uk-margin">
+                <label class="uk-form-label" for="form-stacked-text">Password</label>
+                <div class="uk-form-controls">
+                    <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="password">
+                </div>
+            </div>
+
+            <div class="uk-margin">
+                <button class="uk-button uk-button-default" type="submit">Login</button>
+            </div>
+
+        </form>
+    </div>
+</div>
+
 <script src='https://cdnjs.com/cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 <script src='https://cdnjs.com/cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit.min.js'></script>
 <script src='https://cdnjs.com/cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/js/uikit-icons.min.js'></script>
